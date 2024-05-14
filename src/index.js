d78708ef518266914a443e019c279f9cad3c2bb1
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-05-14 18:38:04 +0000

    try5

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 // ReactDOM.render(<App/>, document.getElementById("root"));
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
